b92900961871b99ae2374ab1c63e2131d64637fe
Jacob Teves <jacobteves@gmail.com>
2016-04-22 22:21:37 -0400

    File /C:/Users/jacobteves/workspace/eclipse/Assignment4/src/RectangleDiskTest.java changed at Fri Apr 22 22:21:36 EDT 2016

--- src/RectangleDiskTest.java
+++ src/RectangleDiskTest.java
@@ -22,9 +22,10 @@ public class RectangleDiskTest extends TestCase {
      * @throws IOException 
      */
     public void testBadParams() throws IOException {
+    	RectangleDisk rd = new RectangleDisk());
         String[] params = { "DrewTest.txt", "dataFile.dat", "5", "4096" };
         RectangleDisk.main(params);
-        assertNotNull(new RectangleDisk());
+        assertNotNull(rd);
     }
 
 //    /**
