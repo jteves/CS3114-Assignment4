c539239da345638641cb990a01008a1ef5bbfbe7
Jacob Teves <jacobteves@gmail.com>
2016-04-22 22:19:08 -0400

    File /C:/Users/jacobteves/workspace/eclipse/Assignment4/src/MemManTest.java changed at Fri Apr 22 22:19:07 EDT 2016

--- src/MemManTest.java
+++ src/MemManTest.java
@@ -1,20 +1,23 @@
 import student.TestCase;
 
 /**
- * The test case for the KVPair class
+ * The test case for the MemMan class
  * @author Jacob Teves, Drew Williams
  * @version 3/14/2016
  */
 public class MemManTest extends TestCase {
 
-	public void testMemMan() {
-		MemMan m = new MemMan(1000);
-		
-		m.add(10);
-		
-		m.dump();
-		
-		String expectedOutput = "Freelist Blocks:\n(10, 990)\n";
-		assertEquals(expectedOutput, systemOut().getHistory());
-	}
+	/**
+	 * Test for MemMan
+	 */
+    public void testMemMan() {
+        MemMan m = new MemMan(1000);
+        
+        m.add(10);
+        
+        m.dump();
+        
+        String expectedOutput = "Freelist Blocks:\n(10, 990)\n";
+        assertEquals(expectedOutput, systemOut().getHistory());
+    }
 }
