763ade5d2e2f98ca23296dfb20d39ed322a0b1de
Andrew Williams <dwill225@vt.edu>
2016-04-28 17:56:06 -0400

    File /C:/Users/Drew/Documents/CS3114/Assignment4/src/RectangleDiskTest.java changed at Thu Apr 28 17:56:06 EDT 2016

--- src/RectangleDiskTest.java
+++ src/RectangleDiskTest.java
@@ -24,7 +24,7 @@ public class RectangleDiskTest extends TestCase {
      */
     public void testBadParams() throws IOException, ClassNotFoundException {
         RectangleDisk rd = new RectangleDisk();
-        String[] params = { "DrewTest.txt", "dataFile.dat", "5", "4096" };
+        String[] params = { "DrewTest.txt", "dataFile.dat", "5", "4096"};
         RectangleDisk.main(params);
         //assertNotNull(rd);
     }
